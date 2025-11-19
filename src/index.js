ca73f9f8ba8173c506934707eb11b5f776540072
Dimasik B <dimasikb@example.com>
2025-11-19 12:54:56 +0300

    Dynamic ul

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const header = "Какое-то значение";
 const firstName = "Дима";
 const lastName = "Б.";
 const items = ["Элемент 1", "Элемент 2", "Элемент 3", "Элемент 4", "Элемент 5", "Элемент 6"]
+const getRandom = () => {
+  return Math.floor(Math.random() * 10);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,12 +16,10 @@ root.render(
     <h1>{header}</h1>
     <h1>{firstName} {lastName}</h1>
     <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
-      <li>{items[3]}</li>
-      <li>{items[4]}</li>
-      <li>{items[5]}</li>
+      {
+        // items.map(item => (<li>{item}</li>))
+        Array.from({ length: 5 }, (_, index) => <li>Item [{getRandom() % 2 === 0 ? 'EVEN' : 'ODD'}]</li>)
+      }
     </ul>
     <p>&copy; Авторские права мои {new Date().getFullYear()}</p>
   </div>
