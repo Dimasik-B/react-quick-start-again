3d5da03490215ddd322d251d648de8eb5544344b
Dimasik B <dimasikb@example.com>
2025-11-19 12:40:44 +0300

    Prctice 2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const header = "Какое-то значение";
+const firstName = "Дима";
+const lastName = "Б.";
+const items = ["Элемент 1", "Элемент 2", "Элемент 3", "Элемент 4", "Элемент 5", "Элемент 6"]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Какой-то текст</h1>
+    <h1>{header}</h1>
+    <h1>{firstName} {lastName}</h1>
     <ul>
-      <li>Lorem ipsum dolor sit amet.</li>
-      <li>Molestias ad eius adipisci exercitationem.</li>
-      <li>Quas similique est iste libero!</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+      <li>{items[3]}</li>
+      <li>{items[4]}</li>
+      <li>{items[5]}</li>
     </ul>
+    <p>&copy; Авторские права мои {new Date().getFullYear()}</p>
   </div>
 
 );
