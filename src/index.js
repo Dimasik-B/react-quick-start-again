f4ed94bf3a9bec7a3396a818f68de48b44b10a14
Dimasik B <dimasikb@example.com>
2025-11-27 14:59:13 +0300

    useState useEffect demo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import HomePage from './Layout/HomePage/HomePage';
+import MyComponents from './Layout/Hooks/MyComponents';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  // <MyComponents />
   <HomePage />
 );
 
